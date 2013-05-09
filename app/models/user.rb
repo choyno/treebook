8ed589021ddd881f4e762d14f3633c8da76654ad
choyno <ariel.lopezf@gmail.com>
2013-05-10 05:27:13 +0800

    adding gravatat

--- app/models/user.rb
+++ app/models/user.rb
@@ -26,4 +26,14 @@ class User < ActiveRecord::Base
    "#{first_name}" + " " + "#{last_name}" 
   end
 
+
+  def gravatar_url
+    stripped_email = email.strip #remove white spaces
+    downcase_email = stripped_email.downcase
+
+    hash = Digest::MD5.hexdigest(downcase_email)
+
+    "http://www.gravatar.com/avatar/#{hash}"
+  end
+
 end
